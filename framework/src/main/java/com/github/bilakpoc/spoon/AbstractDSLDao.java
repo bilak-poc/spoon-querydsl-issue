de3011c6b56b0a28146e8d9c5524d13d8078331e
Lukas Vasek <bilaak@gmail.com>
2018-06-04 13:54:46 +0200

    next issue Cannot provide CtType for com.mysema.query.types.expr.ComparableExpressionBase

--- framework/src/main/java/com/github/bilakpoc/spoon/AbstractDSLDao.java
+++ framework/src/main/java/com/github/bilakpoc/spoon/AbstractDSLDao.java
@@ -6,7 +6,11 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
 
 import com.mysema.query.sql.RelationalPath;
+import com.mysema.query.types.ConstantImpl;
+import com.mysema.query.types.Ops;
 import com.mysema.query.types.PathMetadataFactory;
+import com.mysema.query.types.expr.StringExpression;
+import com.mysema.query.types.expr.StringOperation;
 import com.mysema.query.types.path.StringPath;
 
 public abstract class AbstractDSLDao {
@@ -19,6 +23,10 @@ public abstract class AbstractDSLDao {
 
     public static void testQueryDslIssue(){
         QTag.tag.tagcode.asc();
+    }
+
+    protected StringExpression stringToUpperCaseExpression(final String stringToUpperCase) {
+        return StringOperation.create(Ops.UPPER, ConstantImpl.create(stringToUpperCase));
     }
 
     public void setValidator(Validator validator) {
