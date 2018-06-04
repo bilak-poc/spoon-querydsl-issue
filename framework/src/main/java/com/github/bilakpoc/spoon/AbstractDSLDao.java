7273a6dcba1cc47fc22d97750a3ccdc5777ca65a
Lukas Vasek <bilaak@gmail.com>
2018-06-04 10:14:16 +0200

    issue with no visibility of javax.validation.ValidatorFactory

--- framework/src/main/java/com/github/bilakpoc/spoon/AbstractDSLDao.java
+++ framework/src/main/java/com/github/bilakpoc/spoon/AbstractDSLDao.java
@@ -1,13 +1,33 @@
 package com.github.bilakpoc.spoon;
 
+import javax.validation.Validator;
+
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.validation.beanvalidation.SpringValidatorAdapter;
+
 import com.mysema.query.sql.RelationalPath;
 import com.mysema.query.types.PathMetadataFactory;
 import com.mysema.query.types.path.StringPath;
 
 public abstract class AbstractDSLDao {
 
+    private Validator validator;
+
     protected StringPath rowid(RelationalPath<?> parent) {
           return new StringPath(PathMetadataFactory.forProperty(parent, "ROWID"));
+    }
+
+    public void setValidator(Validator validator) {
+        // Unwrap to the native Validator with forExecutables support
+        if (validator instanceof LocalValidatorFactoryBean) {
+            this.validator = ((LocalValidatorFactoryBean) validator).getValidator();
+        }
+        else if (validator instanceof SpringValidatorAdapter) {
+            this.validator = validator.unwrap(Validator.class);
+        }
+        else {
+            this.validator = validator;
+        }
     }
 
 }
